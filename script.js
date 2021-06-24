cc8ff50100d21b8e38011f0c9d8c5022eb428af5
ToroChu <whileonrails@gmail.com>
2021-06-24 23:16:55 +0800

    faq, progress page update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Modal
 const loginBtn = document.querySelector(".login-btn");
 const loginClose = document.querySelector(".login-close");
 const regClose = document.querySelector(".reg-close");
@@ -13,3 +14,15 @@ loginBtn.addEventListener("click", toggleModal);
 regBtn.addEventListener("click", toggleModal);
 loginClose.addEventListener("click", toggleModal);
 regClose.addEventListener("click", toggleModal);
+
+// Accordion
+let previousElement = null;
+document
+  .querySelectorAll(".accordion-card > input[type=radio]")
+  .forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      previousElement && previousElement.classList.toggle("active-accordion");
+      e.target.parentNode.classList.toggle("active-accordion");
+      previousElement = e.target.parentNode;
+    });
+  });
